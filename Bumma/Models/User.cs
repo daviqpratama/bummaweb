cd65652b1d6b29eeba72cd92592d088080b7af91
daviqpratama <daviqpratama28@gmail.com>
2025-06-16 16:42:33 +0700

    login fixed

---
+++ Bumma/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Bumma.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+    }
+}
